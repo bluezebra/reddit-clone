ebcedd8ec5586555350099b1889018208ea77b81
Stephen Montgomery <stephen@zebrafish.biz>
2017-08-06 12:45:34 +0300

    follow until 'Connecting The App With Firebase'
    https://www.sitepoint.com/reddit-clone-react-firebase/

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDAHJzk4fLDKvDbSyDrRMDOubhjbIgxr5g",
+    authDomain: "reddit-clone-11ab0.firebaseapp.com",
+    databaseURL: "https://reddit-clone-11ab0.firebaseio.com",
+    projectId: "reddit-clone-11ab0",
+    storageBucket: "reddit-clone-11ab0.appspot.com",
+    messagingSenderId: "119050987604"
+};
+
+
